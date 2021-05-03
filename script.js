a7084baf0acd80a0a6b4f2bea883996f5627c69a
Adt106147 <56335826+Adt106147@users.noreply.github.com>
2021-05-03 23:02:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+var s = skrollr.init()
+
+$(window).scroll(function(evt){
+  if ($(window).scrollTop()>0)
+    $(".navbar").removeClass("navbar-top");
+  else
+      $(".navbar").addClass("navbar-top");
+});
+document.querySelector(".popup-btn").onclick = function () {
+  document.querySelector(".popup").classList.add("display");
+};
+
+document.querySelector(".cancel-btn").onclick = function () {
+  document.querySelector(".popup").classList.remove("display");
+};
+
+document.querySelector(".popup-btn1").onclick = function () {
+  document.querySelector(".popup1").classList.add("display");
+};
+
+document.querySelector(".cancel-btn1").onclick = function () {
+  document.querySelector(".popup1").classList.remove("display");
+};
+
+document.querySelector(".popup-btn2").onclick = function () {
+  document.querySelector(".popup2").classList.add("display");
+};
+
+document.querySelector(".cancel-btn2").onclick = function () {
+  document.querySelector(".popup2").classList.remove("display");
+};
+
+document.querySelector(".popup-btn3").onclick = function () {
+  document.querySelector(".popup3").classList.add("display");
+};
+
+document.querySelector(".cancel-btn3").onclick = function () {
+  document.querySelector(".popup3").classList.remove("display");
+};
+document.querySelector(".popup-btn4").onclick = function () {
+  document.querySelector(".popup4").classList.add("display");
+};
+
+document.querySelector(".cancel-btn4").onclick = function () {
+  document.querySelector(".popup4").classList.remove("display");
+};
+
+document.querySelector(".popup-btn5").onclick = function () {
+  document.querySelector(".popup5").classList.add("display");
+};
+
+document.querySelector(".cancel-btn5").onclick = function () {
+  document.querySelector(".popup5").classList.remove("display");
+};
+
+document.querySelector(".popup-btn6").onclick = function () {
+  document.querySelector(".popup6").classList.add("display");
+};
+
+document.querySelector(".cancel-btn6").onclick = function () {
+  document.querySelector(".popup6").classList.remove("display");
+};
+
+document.querySelector(".popup-btn7").onclick = function () {
+  document.querySelector(".popup7").classList.add("display");
+};
+
+document.querySelector(".cancel-btn7").onclick = function () {
+  document.querySelector(".popup7").classList.remove("display");
+};
+document.querySelector(".popup-btn8").onclick = function () {
+  document.querySelector(".popup8").classList.add("display");
+};
+
+document.querySelector(".cancel-btn8").onclick = function () {
+  document.querySelector(".popup8").classList.remove("display");
+};
+
+document.querySelector(".popup-btn9").onclick = function () {
+  document.querySelector(".popup9").classList.add("display");
+};
+
+document.querySelector(".cancel-btn9").onclick = function () {
+  document.querySelector(".popup9").classList.remove("display");
+};
+document.querySelector(".popup-btn10").onclick = function () {
+  document.querySelector(".popup10").classList.add("display");
+};
+
+document.querySelector(".cancel-btn10").onclick = function () {
+  document.querySelector(".popup10").classList.remove("display");
+};
+document.querySelector(".popup-btn11").onclick = function () {
+  document.querySelector(".popup11").classList.add("display");
+};
+
+document.querySelector(".cancel-btn11").onclick = function () {
+  document.querySelector(".popup11").classList.remove("display");
+};
+document.querySelector(".popup-btn12").onclick = function () {
+  document.querySelector(".popup12").classList.add("display");
+};
+
+document.querySelector(".cancel-btn12").onclick = function () {
+  document.querySelector(".popup12").classList.remove("display");
+};
